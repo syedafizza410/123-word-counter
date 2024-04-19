1053257afd3f9688a0bcd90c985adccb2b383f61
syedafizza410 <162727504+syedafizza410@users.noreply.github.com>
2024-04-20 02:18:50 +0500

    add a change

---
+++ index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import inquirer from 'inquirer';
+console.log("******Welcome To My Word Counter Project******");
+const answers = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter your sentence to count the word: "
+    }
+]);
+const words = answers.Sentence.trim().split(" ");
+console.log(words);
+console.log(`Your sentence word count is ${words.length}`);
